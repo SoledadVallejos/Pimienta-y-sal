2c88bfd5e41ace934986bddfa0cc1510827c4551
Soledad Vallejos <soledad_vallejos@hotmail.com>
2021-09-24 12:09:18 -0300

    se agrega controllers, routes y archivo json

---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+const products = require('../data/products.json');
+module.exports = {
+    index : ( req,res) => res.render('index',{products}),
+    detail : (req,res) => res.render('detalleMenu')
+}
